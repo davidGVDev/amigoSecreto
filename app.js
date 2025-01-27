5154f74191f027ba8eb4a8dc18598838ba9feedd
David <david38ing@gmail.com>
2025-01-27 08:15:56 -0600

    Challange finlizado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+const agregarAmigo = () => {
+  const nombre = document.getElementById("amigo").value;
+  if (nombre.trim() === "") {
+    alert("Por favor, inserte un nombre");
+    return;
+  }
+  amigos.push(nombre.trim());
+  console.log(amigos);
+
+  const list = document.getElementById("listaAmigos");
+  list.innerHTML = "";
+  amigos.forEach((amigo) => {
+    list.innerHTML += `<li>${amigo}</li>`;
+  });
+  document.getElementById("amigo").value = "";
+};
+
+const sortearAmigo = () => {
+  document.getElementById("listaAmigos").setAttribute("style", "display: none;");
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear");
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[randomIndex];
+  document.getElementById(
+    "resultado"
+  ).innerHTML = `El amigo sorteado es ${amigoSorteado}`;
+};
